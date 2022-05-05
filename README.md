8705f4f3267e9b0c4cf689654e3fef1890374670
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-05-04 23:42:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# data_analyze_front_end
+ 
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:data_analyze/modules/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
