2e4e18f6a2cdd0f12cf000f7544fd0ba01728c4a
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-05-11 23:59:49 -0300

    adicao de alguns packages necessarios

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Observer(
+        builder: (_) {
+          return Column(
+            children: const [],
+          );
+        },
+      ),
+    );
   }
 }
