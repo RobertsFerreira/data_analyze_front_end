3ce455d4c1e5f7d5add34027db3428e576447db5
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-07-06 00:09:37 -0300

    preparo da funcao de uplaod

--- lib/modules/manipulation_file/datasource/manipulation_file_datasource.dart
+++ lib/modules/manipulation_file/datasource/manipulation_file_datasource.dart
@@ -1,31 +1,68 @@
+import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:data_analyze/shared/clients/dio_client_http.dart';
 import 'package:fpdart/fpdart.dart';
 
 import '../../../models/error/exception/generic_exception.dart';
 import '../models/error/file_errors.dart';
+import '../repository/manipulation_file_repository.dart';
 
 class ManipulationFileDataSource {
+  final ManipulationFileRepository repository = ManipulationFileRepository(
+    DioClientHttp(),
+  );
+
+  Future<Either<GenericException, void>> uploadFile(
+    Uint8List fileB64, {
+    String? filename,
+  }) async {
+    try {
+      final result = await repository.verifyStatusApi();
+      result.fold(
+        (l) => throw (l),
+        (r) => log(r.toString()),
+      );
+      return const Right(null);
+    } catch (e) {
+      return Left(
+        UnknownError(
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
   Either<GenericException, File> openFile(String path) {
     try {
       final file = File(path);
       if (file.existsSync()) {
         return Right(file);
       } else {
-        throw Left(
-          FileNotFound(
-            message: 'Caminho do arquivo escolhido não existe!',
-            keys: ['path'],
-            datas: [path],
-          ),
-        );
+        throw FileNotFound(message: 'Caminho do arquivo escolhido não existe!');
       }
-    } catch (e) {
+    } on FileNotFound catch (error, stackTrace) {
+      return Left(
+        FileNotFound(
+          message: error.message,
+          keys: ['path', 'error', 'stackTrace'],
+          datas: [
+            path,
+            error.toString(),
+            stackTrace.toString(),
+          ],
+        ),
+      );
+    } catch (error, stackTrace) {
       return Left(
         UnknownError(
           message: 'Erro desconhecido',
-          keys: ['error'],
-          datas: [e.toString()],
+          keys: ['error', 'stackTrace'],
+          datas: [
+            error.toString(),
+            stackTrace.toString(),
+          ],
         ),
       );
     }
