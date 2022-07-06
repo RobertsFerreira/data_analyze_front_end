fae4ce0814e61b33d1b44a4eb3a1634037a9e46a
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-07-06 00:10:14 -0300

    tratamento de erro caso o usuario feche a escolha do file

--- lib/modules/manipulation_file/pages/manipulation_file_page.dart
+++ lib/modules/manipulation_file/pages/manipulation_file_page.dart
@@ -1,7 +1,10 @@
 import 'package:data_analyze/modules/manipulation_file/datasource/manipulation_file_datasource.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../controller/manipulation_file_controller.dart';
+import '../models/error/file_errors.dart';
 
 class ManipulationFile extends StatefulWidget {
   const ManipulationFile({Key? key}) : super(key: key);
@@ -40,15 +43,49 @@ class _ManipulationFileState extends State<ManipulationFile> {
                 ),
               ),
               const SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: controller.openFile,
-                child: const Text('Selecionar arquivo'),
+              Observer(
+                builder: (_) {
+                  dynamic error = controller.error;
+                  return ElevatedButton(
+                    onPressed: () async {
+                      await controller.openFile();
+                      error = controller.error;
+                      if (error != null && error is FileSelectError) {
+                        showDialog(
+                          context: context,
+                          builder: (ctx) {
+                            return AlertDialog(
+                              title: Text(error.message),
+                              content: const Text('Tentar Novamente!!!'),
+                              actions: [
+                                ElevatedButton(
+                                  onPressed: () => Navigator.of(ctx).pop(),
+                                  child: const Text('Não'),
+                                ),
+                                ElevatedButton(
+                                  onPressed: controller.openFile,
+                                  child: const Text('Sim'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: const Text('Selecionar arquivo'),
+                  );
+                },
               ),
               const SizedBox(width: 10),
             ],
           ),
           const Divider()
         ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: controller.verifyStatusApi,
+        label: const FaIcon(FontAwesomeIcons.fileArrowUp),
+        tooltip: 'Upload de arquivo',
       ),
     );
   }
