36bc41c411d67680bd0991ab003ef1b1ef343bee
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-07-06 00:10:34 -0300

    adicao da funcao de status da api

--- lib/modules/manipulation_file/controller/manipulation_file_controller.dart
+++ lib/modules/manipulation_file/controller/manipulation_file_controller.dart
@@ -1,11 +1,14 @@
 import 'dart:developer';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:data_analyze/modules/manipulation_file/datasource/manipulation_file_datasource.dart';
+import 'package:data_analyze/modules/manipulation_file/models/error/file_errors.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 
+import '../../../models/error/exception/generic_exception.dart';
 import '../../../utils/async_function.dart';
 
 part 'manipulation_file_controller.g.dart';
@@ -22,23 +25,70 @@ abstract class _ManipulationFileControllerBase with Store {
   });
 
   @observable
-  File? path;
+  File? file;
+
+  @observable
+  dynamic error;
+
+  @observable
+  Uint8List file64 = Uint8List(1);
 
   @action
-  void openFile() => asyncAction(
-        () async {
-          final fileResult = await FilePicker.platform.pickFiles(
-            withData: true,
-            allowedExtensions: ['csv'],
-            type: FileType.custom,
-          );
-          if (fileResult != null) {
-            final path = fileResult.files.first.path;
+  void setFileB64(Uint8List bytes) => file64 = bytes;
 
-            final result = dataSource.openFile(path ?? '');
-            result.fold(
-              (l) => log(l.message),
-              (r) => textEditingController.text = r.path,
+  @action
+  void verifyStatusApi() => asyncAction(
+        () async {
+          await dataSource.uploadFile(
+            file64,
+            filename: file?.path,
+          );
+        },
+      );
+
+  @action
+  Future<void> openFile() => asyncAction(
+        () async {
+          error = null;
+          try {
+            final fileResult = await FilePicker.platform.pickFiles(
+              dialogTitle: 'Selecionar Arquivo CSV',
+              withData: true,
+              allowedExtensions: ['csv'],
+              type: FileType.custom,
+              lockParentWindow: true,
+            );
+            if (fileResult != null) {
+              final bytes = fileResult.files.first.bytes!;
+              setFileB64(bytes);
+              final path = fileResult.files.first.path;
+
+              final result = dataSource.openFile(path ?? '');
+              result.fold(
+                (l) => throw (l),
+                (r) => textEditingController.text = r.path,
+              );
+            } else {
+              throw FileSelectError(message: "Nenhum arquivo selecionado!!!");
+            }
+          } on FileSelectError catch (e, s) {
+            error = e;
+            log(
+              e.message,
+              error: e,
+              stackTrace: s,
+            );
+          } on FileNotFound catch (e, s) {
+            log(
+              e.message,
+              error: e,
+              stackTrace: s,
+            );
+          } on UnknownError catch (e, s) {
+            log(
+              e.message,
+              error: e,
+              stackTrace: s,
             );
           }
         },
--- lib/modules/manipulation_file/controller/manipulation_file_controller.g.dart
+++ lib/modules/manipulation_file/controller/manipulation_file_controller.g.dart
@@ -9,19 +9,51 @@ part of 'manipulation_file_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$ManipulationFileController on _ManipulationFileControllerBase, Store {
-  late final _$pathAtom =
-      Atom(name: '_ManipulationFileControllerBase.path', context: context);
+  late final _$fileAtom =
+      Atom(name: '_ManipulationFileControllerBase.file', context: context);
 
   @override
-  File? get path {
-    _$pathAtom.reportRead();
-    return super.path;
+  File? get file {
+    _$fileAtom.reportRead();
+    return super.file;
   }
 
   @override
-  set path(File? value) {
-    _$pathAtom.reportWrite(value, super.path, () {
-      super.path = value;
+  set file(File? value) {
+    _$fileAtom.reportWrite(value, super.file, () {
+      super.file = value;
+    });
+  }
+
+  late final _$errorAtom =
+      Atom(name: '_ManipulationFileControllerBase.error', context: context);
+
+  @override
+  dynamic get error {
+    _$errorAtom.reportRead();
+    return super.error;
+  }
+
+  @override
+  set error(dynamic value) {
+    _$errorAtom.reportWrite(value, super.error, () {
+      super.error = value;
+    });
+  }
+
+  late final _$file64Atom =
+      Atom(name: '_ManipulationFileControllerBase.file64', context: context);
+
+  @override
+  Uint8List get file64 {
+    _$file64Atom.reportRead();
+    return super.file64;
+  }
+
+  @override
+  set file64(Uint8List value) {
+    _$file64Atom.reportWrite(value, super.file64, () {
+      super.file64 = value;
     });
   }
 
@@ -30,7 +62,29 @@ mixin _$ManipulationFileController on _ManipulationFileControllerBase, Store {
           name: '_ManipulationFileControllerBase', context: context);
 
   @override
-  void openFile() {
+  void setFileB64(Uint8List bytes) {
+    final _$actionInfo = _$_ManipulationFileControllerBaseActionController
+        .startAction(name: '_ManipulationFileControllerBase.setFileB64');
+    try {
+      return super.setFileB64(bytes);
+    } finally {
+      _$_ManipulationFileControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void verifyStatusApi() {
+    final _$actionInfo = _$_ManipulationFileControllerBaseActionController
+        .startAction(name: '_ManipulationFileControllerBase.verifyStatusApi');
+    try {
+      return super.verifyStatusApi();
+    } finally {
+      _$_ManipulationFileControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  Future<void> openFile() {
     final _$actionInfo = _$_ManipulationFileControllerBaseActionController
         .startAction(name: '_ManipulationFileControllerBase.openFile');
     try {
@@ -43,7 +97,9 @@ mixin _$ManipulationFileController on _ManipulationFileControllerBase, Store {
   @override
   String toString() {
     return '''
-path: ${path}
+file: ${file},
+error: ${error},
+file64: ${file64}
     ''';
   }
 }
