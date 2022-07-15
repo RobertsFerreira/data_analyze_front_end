21e6c659965df85e1c405083cb4e98a3e238eac2
Robert Ferreira <61979077+RobertsFerreira@users.noreply.github.com>
2022-07-15 20:48:51 -0300

    tentativa 2 de corrigir erro

--- test/modules/manipulation_file/datasource/manipulation_file_datasource_test.dart
+++ test/modules/manipulation_file/datasource/manipulation_file_datasource_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:data_analyze/modules/manipulation_file/datasource/manipulation_file_datasource.dart';
+import 'package:data_analyze/modules/manipulation_file/models/error/file_errors.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fpdart/fpdart.dart';
 
@@ -14,22 +15,22 @@ void main() {
         final ManipulationFileDataSource fileDataSource =
             ManipulationFileDataSource();
 
-        final file = fileDataSource.openFile(path);
+        final file = fileDataSource.openFile('assets\\csv\\data_pesquisa3.csv');
 
         expect(file.fold(id, id), isA<File>());
       },
     );
 
-    // test(
-    //   'Deve retornar um erro ao receber um caminho inexistente ',
-    //   () {
-    //     final ManipulationFileDataSource fileDataSource =
-    //         ManipulationFileDataSource();
+    test(
+      'Deve retornar um erro ao receber um caminho inexistente ',
+      () {
+        final ManipulationFileDataSource fileDataSource =
+            ManipulationFileDataSource();
 
-    //     final file = fileDataSource.openFile('$path/a');
+        final file = fileDataSource.openFile('$path/a');
 
-    //     expect(file.fold(id, id), isA<FileNotFound>());
-    //   },
-    // );
+        expect(file.fold(id, id), isA<FileNotFound>());
+      },
+    );
   });
 }
